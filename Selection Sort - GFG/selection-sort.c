a06ef8650fa8cc751162710d4f8bd21ea01b3cb3
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-25 23:29:05 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.c
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int select(int arr[], int i);
+void selectionSort(int arr[], int n);
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+
+
+void selectionSort(int arr[], int n)
+{
+ int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+  
+}
