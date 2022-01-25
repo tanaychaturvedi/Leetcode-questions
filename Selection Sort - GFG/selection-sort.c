6573215565bd4b110d0fe2e8c36ab203f4df7b37
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-25 23:32:14 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.c
+++ Selection Sort - GFG/selection-sort.c
@@ -43,21 +43,22 @@ int main()
 
 
 
-void selectionSort(int arr[], int n)
+void selectionSort(int a[], int n)
 {
- int i, j, min_idx;
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n-1; i++)
-    {
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < n; j++)
-        if (arr[j] < arr[min_idx])
-            min_idx = j;
- 
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
+  int min;
+  for(int i=0;i<n;i++)
+  {
+       min=i;
+      for(int j=i+1;j<n;j++)
+      {
+          if(a[j]<a[min])
+            min=j;
+            
+      }
+     int temp=a[i];
+     a[i]=a[min];
+     a[min]=temp;
+     
+  }
   
 }
