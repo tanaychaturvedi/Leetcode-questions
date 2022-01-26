7f6fa7354a6f8ce8eaf5e95243950551b6d753b4
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-27 00:32:04 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (22.89%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,48 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-	    int idx1 = -1;
-		int idx2 = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                if (idx2 != -1) {
-                    std::swap(nums[i], nums[idx2]);
-                    if (idx1 != -1) {
-                        std::swap(nums[idx2], nums[idx1]);
-                    }
-                } else {
-                    if (idx1 != -1) {
-                        std::swap(nums[i], nums[idx1]);
-                    }
-                }
-                
-                if (idx2 != -1) {
-                    idx2++;
-                }
-                if (idx1 != -1) {
-                    idx1++;
-                }
-            } else if (nums[i] == 1) {
-                if (idx2 != -1) {
-                    std::swap(nums[i], nums[idx2]);
-                }
-                
-                if (idx2 != -1) {
-                    idx2++;
-                }
-                
-                if (idx1 == -1) {
-                    if (idx2 == -1) {
-                        idx1 = i;
-                    } else {
-                        idx1 = idx2 - 1;
-                    }
-                }
-            } else {
-                if (idx2 == -1) {
-                    idx2 = i;
-                }
-            }
-        }
+        sort(nums.begin(),nums.end());
     }
 };
