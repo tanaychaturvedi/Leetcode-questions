a8dcd79cadb579d004b63f857edac697a0527c5f
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-27 00:43:51 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (22.89%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int lo=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            switch(nums[mid])
+            {
+                case 0:
+                    swap(nums[lo++],nums[mid++]);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid],nums[high--]);
+                    break;
+                    
+            
+                    
+                    
+            }
+        }
     }
 };
