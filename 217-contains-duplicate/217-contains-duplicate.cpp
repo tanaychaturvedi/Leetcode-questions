e43ce223edbe83b379b857cdad45c24d5c152957
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-31 16:43:48 +0530

    Time: 173 ms (18.50%), Space: 53.5 MB (16.72%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+       unordered_map<int,int> m;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        for(auto i :m)
+        {
+            if(i.second>=2)
+                return true;
+        }
+        
+    return false;
+    }
+};
