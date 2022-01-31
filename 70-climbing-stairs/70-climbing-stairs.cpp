14e88460ba03745ae1fd24f8cab3b4b2ec8d839b
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-01-31 14:59:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (28.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,13 @@
 class Solution {
-    int fibo(int n)
-    {
-        int a[n];
-        a[0]=1;
-        a[1]=1;
-        for(int i=2;i<n;i++)
-        {
-            a[i]=a[i-1]+a[i-2];
-        }
-        return a[n-1];
-    }
 public:
     int climbStairs(int n) {
-        return fibo(n+1);
+      vector<int> dp(n+3);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
