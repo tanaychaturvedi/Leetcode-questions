de93e86f131701907b87ffbebb7fe7ba37a47435
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-02-05 02:48:07 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (100.00%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,20 +1,12 @@
+
 class Solution {
 public:
+    int helper(int n,int k){
+        if(n==1)
+            return 0;
+        return (helper(n-1,k) + k) % n;
+    }
     int findTheWinner(int n, int k) {
-        queue<int>q;
-        int count=1;
-        for(int i=0; i<n; i++){
-            q.push(i+1);
-        }
-        while(q.size()>1){
-            while(count!=k){
-                q.push(q.front());
-                q.pop();
-                count++;
-            }
-            q.pop();
-            count=1;
-        }
-        return q.front();
+        return helper(n,k)+1;   //+1 is for converting 0-based indexing to 1-based indexing
     }
 };
