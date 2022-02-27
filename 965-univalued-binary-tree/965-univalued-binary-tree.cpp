87098af137be5ff446e992582531fa1ff3165103
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-02-27 11:20:49 +0530

    Time: 3 ms (68.10%), Space: 9.9 MB (80.01%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.cpp
+++ 965-univalued-binary-tree/965-univalued-binary-tree.cpp
@@ -1,20 +1,24 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    bool isUnivalTree(TreeNode* root) {
-       if(root==NULL)return true;
-       if(root->left!=NULL)if(root->left->val!=root->val)return false;
-       if(root->right!=NULL)if(root->right->val!=root->val)return false;
-       return isUnivalTree(root->left)&&isUnivalTree(root->right);
-  }
+class Solution
+{
+    public:
+        bool isUnivalTree(TreeNode *root)
+        {
+            if (root == NULL) return true;
+            if (root->left != NULL)
+                if (root->left->val != root->val) return false;
+            if (root->right != NULL)
+                if (root->right->val != root->val) return false;
+            return isUnivalTree(root->left) && isUnivalTree(root->right);
+        }
 };
