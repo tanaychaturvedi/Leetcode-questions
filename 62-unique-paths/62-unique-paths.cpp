2f69705c19f72112a15169dc63035bc0003e5240
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-03-22 16:02:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (59.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,18 @@
 class Solution {
-      int dp[101][101];
-  
-    int solve(int i,int j,int n,int m)
-    {
-        if(i==(n-1) && j==(m-1))
-            return 1;
-        if(i>=n || j>=m )
-            return 0;
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else
-            return dp[i][j]= solve(i+1,j,n,m) +solve(i,j+1,n,m);
-    }
+    int dp[101][101];
 public:
-    int uniquePaths(int m, int n) {
-             int i=0;
-        int j=0;
-        memset(dp,-1,sizeof(dp));
-       return solve(i,j,n,m);
+    int solve(int i, int j, int m , int n) {
+        if(i==m-1 and j==n-1)return 1;
+        if(i>=m or j>=n)return 0;
+        if(dp[i][j]!=-1) {
+            return dp[i][j];
+        }
+        else
+           return dp[i][j]= solve(i+1,j,m,n)+solve(i,j+1,m,n);
         
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return  solve(0,0,m,n);
     }
 };
