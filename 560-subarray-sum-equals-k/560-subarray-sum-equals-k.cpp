05a6ebca5da84ea25fe5f327f5801ec73ac1ab4a
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-03-24 19:05:31 +0530

    Time: 136 ms (32.17%), Space: 36.1 MB (62.31%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int cum=0; // cumulated sum
-        map<int,int> rec; // prefix sum recorder
-        int cnt = 0; // number of found subarray
-        rec[0]++; // to take into account those subarrays that begin with index 0
-        for(int i=0;i<nums.size();i++){
-            cum += nums[i];
-            cnt += rec[cum-k];
-            rec[cum]++;
+       unordered_map<int,int> m;
+        int i=0;
+        int c=0;
+        int count=0;
+    int n=nums.size();
+        while(i<n) {
+            c+=nums[i];
+            if(c==k)count++;
+            if(m.find(c-k)!=m.end()) count+=m[c-k];
+            m[c]++;
+            i++;
         }
-        return cnt;
+        return count;
+        
     }
 };
