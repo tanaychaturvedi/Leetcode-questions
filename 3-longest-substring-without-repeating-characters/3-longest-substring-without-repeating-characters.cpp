09da0c18d26e9c8dc714558005a0254c23e2352a
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-03-24 16:52:21 +0530

    Time: 58 ms (19.78%), Space: 13.5 MB (15.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,31 @@
-class Solution {
-public:
-   int lengthOfLongestSubstring(string s) {
-int start=0,end=0,n=s.length(),max_len=0;
-set<char> st;
-// st.insert(s[0]);
-while(end<n)
+class Solution
 {
-st.insert(s[end]);
-max_len=max(max_len,end-start+1);
-end++;
-if(st.find(s[end])!=st.end())
-{
-while(start<end)
-{
-if(st.find(s[end])!=st.end())
-{
-st.erase(s[start]);
-start++;
-}
-else
-break;
-}
-
+    public:
+        int lengthOfLongestSubstring(string s)
+        {
+            int start = 0, end = 0, n = s.length(), max_len = 0;
+            set<char> st;
+           	// st.insert(s[0]);
+            while (end < n)
+            {
+                st.insert(s[end]);
+                max_len = max(max_len, end - start + 1);
+                end++;
+                if (st.find(s[end]) != st.end())
+                {
+                    while (start < end)
+                    {
+                        if (st.find(s[end]) != st.end())
+                        {
+                            st.erase(s[start]);
+                            start++;
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            max_len = max(max_len, end - start);
+            return max_len;
         }
-        
-    }
-    max_len=max(max_len,end-start);
-    return max_len;
-}
 };
