8d4a992275df694996c5e46f7b43b082efe64687
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-03-24 17:02:06 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (5.57%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& m, int target) {
         for(auto i:m) {
-            for(auto j:i) {
-                if(j==target)return true;
-            }
+           if(binary_search(i.begin(),i.end(),target))return true;
         }
         return false;
     }
