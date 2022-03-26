71083e04371b3561733a7e7c3a4f65de7a5f6b74
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-03-26 22:20:44 +0530

    Time: 38 ms (27.48%), Space: 14.6 MB (57.41%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -5,9 +5,9 @@ public:
         for(auto i:arr) {
             m[i]++;
         }
-        vector<string> v;
+        // vector<string> v;
         int c=0;
-        for(auto i:m)if(i.second==1)v.push_back(i.first);
+        
         for(auto i:arr) {
              if(m[i]==1)c++;
             if(m[i]==1 and c==k){
