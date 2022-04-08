503a81261b7a309fd76d4fe1d492796b738a6e96
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-04-09 02:18:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.83%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    int getSum(int a, int b) {
-        return a+b;
+   int getSum(int a, int b) 
+    {
+        int sum=a;
+        uint32_t carry=b;
+        
+        while(carry)
+        {
+            sum=a^carry;
+            carry=(a&carry)<<1;
+            a=sum;
+        }
+        
+        return sum;
     }
 };
