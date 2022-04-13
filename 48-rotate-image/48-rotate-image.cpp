2c6c562b4ddeac74e57bf7313f7a35e8ff9cc8a0
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-04-14 01:44:40 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.04%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& m) {
-       
-        int n=m.size();
-        for(int i=0;i<n;i++) {
+    void rotate(vector<vector<int>>& g) {
+        
+        int m=g.size();
+        int n=g[0].size();
+        for(int i=0;i<m;i++) {
             for(int j=0;j<i;j++) {
-                swap(m[i][j],m[j][i]);
-                
+                swap(g[i][j],g[j][i]);
             }
         }
-        
-        for(int i=0;i<n;i++) {
-            reverse(m[i].begin(),m[i].end());
+      for(int i=0;i<m;i++) {
+            reverse(g[i].begin(),g[i].end());
         }
+        return;
     }
 };
