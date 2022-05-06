44ce4cb54b6eb6e2f79f2507267ff821fe089b46
Tanay Chaturvedi <55832698+tanaychaturvedi@users.noreply.github.com>
2022-05-06 22:40:32 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.25%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1)return true;
+        long long l=1;
+        long long h=num;
+        
+        while(l<=h) {
+            long long mid=(l+h)/2;
+            long long sq=mid*mid;
+            if(sq==num) return true;
+            else if(sq<num) {
+                l=mid+1;
+                
+            }
+            else {
+                h=mid-1;
+            }
+            
+        }
+        return false;
+        
+    }
+};
